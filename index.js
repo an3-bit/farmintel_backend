805b18c1b4cd44fb98719d845e572e4b22ceed27
an3-bit <andrewmogambi12@gmail.com>
2025-08-20 10:25:45 +0300

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'https://farmintel-frontend.vercel.app',
-    'http://localhost:8081'
+    'http://localhost:8080'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
